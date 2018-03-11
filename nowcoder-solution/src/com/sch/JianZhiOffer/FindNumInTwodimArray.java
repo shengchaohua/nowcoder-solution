1328e854cd73a5e045243b26636a8b3575c9b08a
shengchaohua <shengchaohua163@163.com>
2018-03-11 12:45:19 +0800

    add FindNumInTwoDimArray.java

---
+++ nowcoder-solution/src/com/sch/JianZhiOffer/FindNumInTwodimArray.java
@@ -0,0 +1,26 @@
+package com.sch.JianZhiOffer;
+
+public class FindNumInTwodimArray {
+	public boolean Find(int target, int [][] array) {
+        int row = array.length;
+        int column = array[0].length;
+        for (int i = row-1, j = 0; i >= 0 && j < column ; ) {
+            if (array[i][j] == target) {
+                return true;
+            } else if(array[i][j] > target) {
+                i--;
+            } else {
+                j++;
+            }
+        }
+        return false;
+    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FindNumInTwodimArray fn = new FindNumInTwodimArray();
+		int[][] array = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+		System.out.println(fn.Find(5, array));
+	}
+
+}
